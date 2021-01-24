c0a16bfe182e19cd1742063055351336945969e5
Abner J Pelisser <abner.pelisser@gmail.com>
2021-01-24 16:18:14 -0300

    Endpoint /clientes/{id} disponível

--- src/main/java/com/abner/CursomcApplication.java
+++ src/main/java/com/abner/CursomcApplication.java
@@ -81,11 +81,13 @@ public class CursomcApplication implements CommandLineRunner{
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 		Cliente cli1 = new Cliente(null, "maria silva", "maria.silva@email.com", "11609581000260", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("187332", "93838393"));
+
 		Endereco e1 = new Endereco(null, "Rua flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Rua Matos", "105", "Sala 800", "Jardim", "38220834", cli1, c2);
 		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
 		
-		cli1.getTelefones().addAll(Arrays.asList("187332", "93838393"));
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 		
